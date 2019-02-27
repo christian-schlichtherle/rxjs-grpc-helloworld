901d9c3b0e1c201962cf9d0932fd8d7b1c09b93f
Christian Schlichtherle <christian@schlichtherle.de>
2019-02-27 19:08:57 +0400

    Updates to latest versions.

--- server.ts
+++ server.ts
@@ -1,7 +1,11 @@
-import { Observable } from 'rxjs';
+import { Observable, of, timer } from 'rxjs';
+import { mapTo, take } from 'rxjs/operators'
 import { serverBuilder } from 'rxjs-grpc';
 
 import { helloworld } from './grpc-namespaces';
+import HelloRequest = helloworld.HelloRequest;
+import HelloReply = helloworld.HelloReply;
+import MultiHelloRequest = helloworld.MultiHelloRequest;
 
 async function main() {
   type ServerBuilder = helloworld.ServerBuilder;
@@ -9,16 +13,17 @@ async function main() {
 
   server.addGreeter({
 
-    sayHello(request) {
-      return Observable.of({
+    sayHello(request: HelloRequest): Observable<HelloReply> {
+      return of({
         message: 'Hello ' + request.name
       });
     },
 
-    sayMultiHello(request) {
-      return Observable.timer(100, 500)
-        .mapTo({ message: `Hello ${request.name}` })
-        .take(request.num_greetings);
+    sayMultiHello(request: MultiHelloRequest): Observable<HelloReply> {
+      return timer(100, 500).pipe(
+        mapTo({ message: `Hello ${request.name}` }),
+        take(request.num_greetings)
+      );
     }
 
   });
